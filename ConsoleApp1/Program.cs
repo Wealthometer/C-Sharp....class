453ecd6258f9d2ef1c26b694b1821461e00049b8
Wealthometer <wealthhajoh87@gmail.com>
2025-11-22 16:19:03 +0100

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -13,17 +13,25 @@ namespace ConsoleApp1
             Console.WriteLine("Hello World");
             //Console.ReadLine();
 
-            Console.WriteLine("   /|");
-            Console.WriteLine("  / |");
-            Console.WriteLine(" /  |");
-            Console.WriteLine("/___|");
-            Console.ReadLine();
+            //Console.WriteLine("   /|");
+            //Console.WriteLine("  / |");
+            //Console.WriteLine(" /  |");
+            //Console.WriteLine("/___|");
+            //Console.ReadLine();
 
             // Variables
             Console.WriteLine("There was once a man named George");
             Console.WriteLine("He was 70 years old");
             Console.WriteLine("He really liked the name George");
             Console.WriteLine("But didn't like being 70");
+            //Console.ReadLine();
+
+            string characterName = "John";
+            int characterAge;
+            characterAge = 35;
+
+            Console.WriteLine("There was once a man named " + characterName);
+            Console.WriteLine("He was " + characterAge + " years old");
             Console.ReadLine();
         }
     }
